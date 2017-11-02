6db602d480a0021d98950ca715961b34ea45ee3f
akshaya <akshaya.sarathy97@gmail.com>
2017-11-02 04:56:40 +0000

    Add partials and destroy button

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -60,6 +60,13 @@ class ArticlesController < ApplicationController
        
        
             
+        def destroy
+            @article = Article.find(params[:id])
+            @article.destroy
+            flas[:notice] = "Article was successfully deleted"
+            redirect_to articles_path
+        end
+        
                
         
         
